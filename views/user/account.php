5f970084bcaf8ab6b11b64fd31d887c437e6869b
khaled-KMH <khaledek767403@gmail.com>
2024-05-09 04:17:55 +0300

    acc

---
+++ views/user/account.php
@@ -0,0 +1,118 @@
+<?php
+require_once('../../models/database.php'); // Database connection
+
+$db = new database;
+$db->openConnection();
+session_start();
+
+if (isset($_POST['submit'])) {
+    // Retrieve form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+    $username = $_POST['username'];
+
+    // Fetch existing user account data from the database
+    $query = "SELECT * FROM user WHERE id=" . $_SESSION['id'];
+    $result = $db->query($query);
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $old_name = $row['first_name'];
+        $old_email = $row['email'];
+        $old_password = $row['password'];
+        $old_phone = $row['phone'];
+        $old_address = $row['address'];
+        $old_username = $row['username'];
+
+        // Check if form values are different from the old values
+        if ($name != $old_name || $email != $old_email || $password != $old_password || $phone != $old_phone || $address != $old_address || $username != $old_username) {
+            // Update the user's account information in the database
+            $query = "UPDATE user SET name='$name', email='$email', password='$password', phone='$phone', address='$address', username='$username' WHERE id=" . $_SESSION['id'];
+            $db->query($query);
+        }
+
+        // Redirect to the user page after updating account information
+        header('Location:index.php');
+        exit();
+    } else {
+        // Handle if user data not found
+        // For example, redirect to an error page or display a message
+    }
+}
+
+// Fetch existing user account data from the database
+$query = "SELECT * FROM user WHERE id=" . $_SESSION['id'];
+$result = $db->query($query);
+if ($result->num_rows == 1) {
+    $row = $result->fetch_assoc();
+    $old_name = $row['first_name'];
+    $old_email = $row['email'];
+    $old_password = $row['password'];
+    $old_phone = $row['phone'];
+    $old_address = $row['address'];
+    $old_username = $row['username'];
+} else {
+    // Handle if user data not found
+    // For example, redirect to an error page or display a message
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Update Account</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/all.min.css">
+  <link rel="stylesheet" href="../css/additems.css">
+  <link href="../css/adminStyle.css" rel="stylesheet" />
+</head>
+<body>
+  
+<div class="container-fluid px-1 py-5 mx-auto">
+    <div class="row d-flex justify-content-center">
+        <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
+            <div class="card">
+                <form action="update_account.php" class="form-card" method="post" enctype="multipart/form-data">
+                    <div class="row justify-content-between text-left">
+                        <div class="form-group col-sm-6 flex-column d-flex"> 
+                          <label class="form-control-label px-3">Full Name<span class="text-danger"> *</span></label> 
+                          <input type="text" id="name" name="name" placeholder="Enter Full Name" value="<?php echo $old_name; ?>" required> 
+                        </div>
+                        <div class="form-group col-sm-6 flex-column d-flex">
+                          <label class="form-control-label px-3">Username<span class="text-danger"> *</span></label> 
+                          <input type="text" id="username" name="username" placeholder="Enter Username" value="<?php echo $old_username; ?>" required> 
+                        </div>
+                    </div>
+                    <div class="row justify-content-between text-left">
+                      <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Email<span class="text-danger"> *</span></label> <input type="email" id="email" name="email" placeholder="Enter Email" value="<?php echo $old_email; ?>" required> </div>
+                      <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Password<span class="text-danger"> *</span></label> <input type="password" id="password" name="password" placeholder="Enter Password" value="<?php echo $old_password; ?>" required> </div>
+                    </div>
+                    <div class="row justify-content-between text-left">
+                      <div class="form-group col-sm-6 flex-column d-flex"> 
+                          <label class="form-control-label px-3">Phone<span class="text-danger"> *</span></label> 
+                          <input type="tel" id="phone" name="phone" placeholder="Enter Phone Number" value="<?php echo $old_phone; ?>" required> 
+                      </div>
+                      <div class="form-group col-sm-6 flex-column d-flex"> 
+                          <label class="form-control-label px-3">Address<span class="text-danger"> *</span></label> 
+                          <input type="text" id="address" name="address" placeholder="Enter Address" value="<?php echo $old_address; ?>" required> 
+                      </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="form-group col-sm-6"> <button type="submit" name="submit" class="button_s btn-block btn-primary">Update Account</button> </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="../js/addItem.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</body>
+</html>
